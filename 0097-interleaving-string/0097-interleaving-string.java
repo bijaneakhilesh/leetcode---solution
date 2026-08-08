45aa357310670061025dc8cb40d6ee817db097fa
Akhilesh Bijane <bijaneakhilesh05@gmail.com>
2026-08-09 00:19:15 +0530

    Time: 3 ms (74.99%), Space: 42.9 MB (90.26%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int n = s1.length();
+        int m = s2.length();
+        if (n + m != s3.length()) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[n + 1][m + 1];
+
+        dp[0][0] = true;
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                int k = i + j - 1;
+                if (i > 0 && dp[i - 1][j]
+                        && s1.charAt(i - 1) == s3.charAt(k)) {
+                    dp[i][j] = true;
+                }
+                if (j > 0 && dp[i][j - 1]
+                        && s2.charAt(j - 1) == s3.charAt(k)) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
